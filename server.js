3925b2004971ab4915eec5f5f8dca491e6826d5f
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-12-02 15:31:10 -0600

    finished mvp

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter');
 const server = express();
-
+server.use(express.json());
+server.use("/api/users", userRouter);
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>hi</h2>`);
 });
 
 //custom middleware
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,134 @@
 const express = require('express');
-
+const Users = require('./userDb')
 const router = express.Router();
+const Post = require("../posts/postDb")
 
-router.post('/', (req, res) => {
-  // do your magic!
+
+
+
+
+router.post('/', validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "user not added" })
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const newPost = req.body
+  Post.insert(newPost)
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "post not added" })
+    })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ message: "nothing to grab" })
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  const { id } = req.params
+  Users.getById(id)
+    .then(e => {
+      res.status(200).json(e)
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "user not found" })
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.params
+  Users.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "no posts bro" })
+    })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  const { id } = req.params
+  Users.remove(id)
+    .then(deletedStuff => {
+      res.status(200).json(deletedStuff)
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "delete not deleted" })
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  const { id } = req.params
+  Users.update(id, req.body)
+    .then(e => {
+      if (e === 1) {
+        res.status(201).json({ message: "updated" })
+      }
+      else {
+        res.status(400).json({ message: "not updated" })
+      }
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).json({ message: "does not compute" })
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params
+  Users.getById(id)
+    .then(userId => {
+      if (userId) {
+        req.id = userId;
+        next()
+      }
+      else {
+        res.status(404).json({ message: "not a valid id dude" })
+      }
+    })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const user = req.body;
+
+  if (!user.name) {
+    res.status(400).json({ message: "name is required " })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const post = req.body;
+
+  if (!post.text) {
+    res.status(400).json({ message: "name is required " })
+  } else {
+    next()
+  }
 }
+
 
 module.exports = router;
